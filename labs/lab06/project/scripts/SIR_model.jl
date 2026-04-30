1568bae9d77b738bbeaa166f894f2b8d102fbd89
wakutaipa <1032239009@rudn.ru>
2026-04-30 14:19:53 +0300

    feat(main): added lab6 files

---
+++ labs/lab06/project/scripts/SIR_model.jl
@@ -0,0 +1,25 @@
+using DifferentialEquations, Plots
+
+N = 12900
+I0 = 190
+R0 = 59
+p = [0.2, 0.4]
+tspan = (0, 50)
+S0 = N -I0 - R0
+X0 = [S0, I0, R0]
+
+function syst(x, p, t)
+	(S, I, R) = x
+	(b, c) = p
+	dS = 0
+	dI = -c*I
+	dR = c*I
+	return [dS, dI, dR]
+end
+
+prob = ODEProblem(syst, X0, tspan, p)
+sol = solve(prob, Tsit5(), saveat = 0.01)
+
+pl = plot(sol, labels=["S(t)" "I(t)" "R(t)"], xlabel="Время", ylabel="Численность популяции")
+
+savefig(pl, "I(0)<=I*.png")
